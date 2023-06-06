dfeea8ffd4fd1f6cf4afd34212f4ec844bdc742e
elaldiaysenur <20194703062@ogr.akdeniz.edu.tr>
2023-06-06 15:21:35 +0300

    React + Redux + Tailwind
    React + Redux + Tailwind ile E-ticaret(E-commerce) Website Clone

---
+++ E-commerce/e-commerce/src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className=" text-center lg:text-left">
+        <div className="p-4 text-center ">
+          © 2023 Copyright:
+          <a
+            className="text-neutral-800"
+            href="https://github.com/elaldiaysenur"
+          > {" "}
+            Ayşe Nur Elaldı
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
